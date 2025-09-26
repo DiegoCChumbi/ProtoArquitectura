522a8f1367596650cb23048b41090943226419ba
DiegoCCH <diego171101@gmail.com>
2025-09-26 11:32:41 -0500

    Implementacion S3 por testear

--- Back/src/s3.js
+++ Back/src/s3.js
@@ -5,9 +5,15 @@ const bucketName = process.env.AWS_S3_BUCKET_NAME;
 const region = process.env.AWS_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const endpoint = process.env.AWS_S3_ENDPOINT;
 
 const s3Client = new S3({
   region: region,
+  endpoint: endpoint,
+  credentials: {
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+  },
 });
 
 async function uploadFIle(fileName, fileBuffer, mimetype) {
